b2cc6779937019733e6e61044c45a2db077aded0
haruncane <erginharuncan@gmail.com>
2022-11-10 22:23:09 +0300

    ödev4g

---
+++ app_server/models/db.js
@@ -0,0 +1,24 @@
+var mongoose = require("mongoose");
+require("./mekansema");
+var dbURI = "mongodb+srv://mekan32:harun39@mekanbul.sxduhj5.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(dbURI);
+function kapat(msg, callback) {
+    mongoose.connection.close(function() {
+        console.log(msg);
+        callback();
+    });
+}
+process.on("SIGINT", function() {
+    kapat("Uygulama kapatıldı!", function() {
+        process.exit(0);
+    });
+});
+mongoose.connection.on("connected", function() {
+    console.log(dbURI + " adresindeki veritabanına bağlanıldı\n");
+});
+mongoose.connection.on("error", function() {
+    console.log("Bağlantı hatası\n");
+});
+mongoose.connection.on("disconnected", function() {
+    console.log(dbURI + " adresindeki veritabanı bağlantısı koptu\n");
+});
